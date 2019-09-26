85b2efb54f3a08109d2417f23f1f412ea09ce7ad
claudriel <claudia.ravasio92@gmail.com>
2019-09-26 20:55:16 +0200

    Fix documenter

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,15 @@ using Documenter, MyPkg
 
 makedocs(;
     modules=[MyPkg],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/algebrato/MyPkg.jl/blob/{commit}{path}#L{line}",
     sitename="MyPkg.jl",
     authors="Stefano Mandelli, Maurizio Tomasi, Silvia Caprioli, Federico Incardona",
-    assets=String[],
 )
 
 deploydocs(;
-    repo="https://github.com/algebrato/MyPkg.jl",
+    repo="github.com/algebrato/MyPkg.jl",
 )
