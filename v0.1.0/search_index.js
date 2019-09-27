cd63a573d5c3b64a1f696d03d2fc6eda659405d2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-09-27 06:49:31 +0000

    build based on f6d38f8

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyPkg.jl-1","page":"Home","title":"MyPkg.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [MyPkg]","category":"page"}]
+}
